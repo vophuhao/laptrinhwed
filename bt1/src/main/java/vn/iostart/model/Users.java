a31fc081d9a7e7ece3366144f5c0537a8d44b8df
vophuhao <vophuhao2004@gmail.com>
2024-09-17 14:42:37 +0700

    tao lop user

---
+++ bt1/src/main/java/vn/iostart/model/Users.java
@@ -0,0 +1,89 @@
+package vn.iostart.model;
+
+import java.io.Serializable;
+
+public class Users implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int userid;
+	private String email;
+	private String userName;
+	private String fullName;
+	private String passWord;
+	private UserRoles roles;
+	private String code;
+	private int roleid;
+	private String phone;
+	public Users(int userid, String email, String userName, String fullName, String passWord, UserRoles roles,
+			String code, int roleid, String phone) {
+		super();
+		this.userid = userid;
+		this.email = email;
+		this.userName = userName;
+		this.fullName = fullName;
+		this.passWord = passWord;
+		this.roles = roles;
+		this.code = code;
+		this.roleid = roleid;
+		this.phone = phone;
+	}
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	public UserRoles getRoles() {
+		return roles;
+	}
+	public void setRoles(UserRoles roles) {
+		this.roles = roles;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public int getRoleid() {
+		return roleid;
+	}
+	public void setRoleid(int roleid) {
+		this.roleid = roleid;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+}
