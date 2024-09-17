1f796a67aa64469905fc8165b95594fb01cce20f
vophuhao <vophuhao2004@gmail.com>
2024-09-17 15:52:21 +0700

    tao connection

--- bt1/src/main/java/vn/iostart/controller/homeController.java
+++ bt1/src/main/java/vn/iostart/controller/homeController.java
@@ -1,5 +1,26 @@
 package vn.iostart.controller;
 
-public class homeController {
+import java.io.IOException;
+import java.rmi.ServerException;
 
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import vn.iostart.service.IUserservice;
+import vn.iostart.service.impl.UserServiceImpl;
+
+@WebServlet(urlPatterns = { "/home", "/login", "/register", "/forgotpass", "/waiting", "/VerifyCode" })
+public class homeController extends HttpServlet{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	IUserservice userService=new UserServiceImpl();
+	
+	protected void doGet(HttpServletRequest req,HttpServletResponse resp) throws ServerException,IOException{
+		
+	}
 }
--- bt1/src/main/java/vn/iostart/dao/DBConnection.java
+++ bt1/src/main/java/vn/iostart/dao/DBConnection.java
@@ -1,13 +1,48 @@
 package vn.iostart.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
-	private final String serverName = "localhost";
-	private final String dbName = "";
-	private final String portNumber = "1433";
-	private final String instance = "";
-	private final String userID = "";
-	private final String password = "";
+	private static String USERNAME = "root";
+	private static String PASSWORD = "1234";
+	private static String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+	private static String URL = "jdbc:mysql://localhost:3306/bt";
+
+	public static Connection getDatabaseConnection() throws SQLException {
+
+		try {
+
+			Class.forName(DRIVER);
+
+		} catch (ClassNotFoundException e) {
+
+			// TODO Auto-generated catch block
+
+			e.printStackTrace();
+
+		}
+
+		return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+	}
+
+	public static void main(String[] args) {
+
+		try {
+
+			new DBConnection();
+
+			System.out.println(DBConnection.getDatabaseConnection());
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+
 
 }
